808b2aa096c0cbaa209acdc8883e4e1364317dfb
Rohit Sinha <rohitsinha707098@gmail.com>
2024-06-03 16:36:33 +0530

    Add Cors config

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require("express");
 const { connectToMongoDB } = require("./database");
 const path = require("path");
 
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "build")));
